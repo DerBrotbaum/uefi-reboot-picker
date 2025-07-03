36fb361de9546303f48cf15dd75259b126fbbafe
Futagirl <anime@incest.rocks>
2025-07-03 21:31:52 +0200

    Initial commit, changed backend from Grub to UEFI, changed name to UEFI Reboot Picker

---
+++ install.sh
@@ -0,0 +1,4 @@
+cp ./src/uefi-reboot-picker.py /bin/uefi-reboot-picker
+cp com.derbrotbaum.uefirebootpicker.desktop /etc/xdg/autostart
+cp com.derbrotbaum.uefirebootpicker.desktop /usr/share/applications
+cp com.derbrotbaum.uefirebootpicker.policy /usr/share/polkit-1/actions
